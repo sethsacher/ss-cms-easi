4975ad3950ca6c2f79244a8d70bcef358834af83
Seth Sacher <ssacher@collabralink.com>
2021-02-09 16:34:03 -0500

    Craco build issue

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,24 @@
 const sassResourcesLoader = require('craco-sass-resources-loader');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
+  webpack: {
+    configure: (webpackConfig) => ({
+      ...webpackConfig,
+      optimization: {
+        ...webpackConfig.optimization,
+        // Workaround for CircleCI bug caused by the number of CPUs shown
+        // https://github.com/facebook/create-react-app/issues/8320
+        minimizer: webpackConfig.optimization.minimizer.map(item => {
+          if (item instanceof TerserPlugin) {
+            item.options.parallel = 0;
+          }
+
+          return item;
+        })
+      },
+    }),
+  },
   plugins: [
     {
       plugin: sassResourcesLoader,
